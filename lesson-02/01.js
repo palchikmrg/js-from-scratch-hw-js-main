834ff7309aad495bfa4620b56d119134af4bf3be
palchikmrg <palchik.mrg@gmail.com>
2025-07-27 17:53:13 +0300

    copmlete task 1 lesson 2

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -16,3 +16,8 @@ const hasTemporaryPass = false
 let isAccess
 
 // your code
+if ((isAdmin === true || isVerifiedUser === true) && (hasSpecialPermission === true || hasTemporaryPass === true)) {
+    isAccess = true;
+} else {
+    isAccess = false;
+}

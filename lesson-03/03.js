909b55adb06e451252849d2682d5edbeba5d3165
palchikmrg <palchik.mrg@gmail.com>
2025-08-02 17:29:14 +0300

    copmlete task 3 lesson 3

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,3 +1,12 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
 
-function findLargest() {}
+function findLargest(number1, number2, number3) {
+    if (number1 > number2 && number1 > number3) {
+        return number1
+    } else if (number2 > number1 && number2 > number3) {
+        return number2
+    } else {
+        return number3
+    }
+}
+

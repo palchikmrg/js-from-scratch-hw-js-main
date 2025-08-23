fdd62c76276e0d4c2ba57b892a304c7c8c1c07f4
palchikmrg <palchik.mrg@gmail.com>
2025-08-24 00:04:32 +0300

    fix_2 task 2 lesson 6

--- lesson-06/02.js
+++ lesson-06/02.js
@@ -28,7 +28,7 @@ const map = (array, callback) => {
 
         let newArray = []
         for (let i = 0; i < array.length; i++) {
-                newArray.push(callback(array[i]));
+                newArray.push(callback(array[i], i));
         }
         return newArray;
 

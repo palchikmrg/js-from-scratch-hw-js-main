90b59554b20286902175957df8a68658c7c2e80f
palchikmrg <palchik.mrg@gmail.com>
2025-08-23 23:26:35 +0300

    copmlete task 1 lesson 6

--- lesson-06/01.js
+++ lesson-06/01.js
@@ -23,4 +23,16 @@ const oddNumbers = filter(numbers, (element, index) => {
 console.log(oddNumbers) // Должен вывести: [1, 3, 5]
 */
 
-const filter = () => {}
+const filter = (array, callback) => {
+    let newArray = []
+    for (let i = 0; i < array.length; i++) {
+        if(callback(array[i], i) === true) {
+            newArray.push(array[i]);
+        }
+
+    }
+    return newArray;
+}
+
+
+
